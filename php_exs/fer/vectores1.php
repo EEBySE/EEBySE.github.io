56c5d9e176da8b34903d291e80f32c3292a684f4
Fer2G <123048581+Fer2G@users.noreply.github.com>
2024-01-22 07:35:03 +0000

    Añadida mi tareita

---
+++ php_exs/fer/vectores1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Input</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>Input de vectores</h1>
+    <hr>
+    <br>
+    <form action="vectores2.php" method="post">
+        <h2>Tamaño del vector A:</h2> 
+        <input type="number" name="a" id="a" required>
+        <br>
+        <h2>Tamaño del vector B:</h2> 
+        <input type="number" name="b" id="b" required>
+        <br>
+        <br>
+        <h4>¿Qué operacion hacemos?</h1>
+        <input type="radio" name="action" id="action" value="1" checked> Suma
+        <br>
+        <input type="radio" name="action" id="action" value="2"> Resta
+        <br>
+        <br>
+        <br>
+        <input type="submit" name="enviar" value="Calcular">
+    </form>
+</body>
+</html>
