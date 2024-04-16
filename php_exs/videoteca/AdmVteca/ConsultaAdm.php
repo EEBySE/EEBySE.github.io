2134a2ffd2a0522ea125f25b0e98cc730dd41c3b
Fer-Jg <76271548+Fer-Jg@users.noreply.github.com>
2024-04-16 14:18:12 -0600

    weitas videoteca final i think

---
+++ php_exs/videoteca/AdmVteca/ConsultaAdm.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>videoteca</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="../images/Refresh.css" type="text/css" /></head>
+<body>
+<div id="wrap">
+  <div id="header">
+    <h1 id="logo-text">videoteca</h1>
+    <h2 id="slogan">Facultad de Ciencias de la Computacion </h2>
+    <p>&nbsp;</p>
+    <form class="search" method="post" action="#">
+      <p>
+        <input class="textbox" type="text" name="search_query" value="" />
+        <input class="button" type="submit" name="Submit" value="Search" />
+      </p>
+    </form>
+  </div>
+  <div  id="menu">
+    <ul>
+      <li><a href="index.html">inicio</a></li>
+      <li id="current"><a href="consultaAdmin.php">Consultas</a></li>
+      <li><a href="crud.php">CRUD</a></li>
+      <li><a href="reportes.php">Reportes</a></li>
+      <li><a href="salir.php">Salir</a></li>
+    </ul>
+  </div>
+  <div id="content-wrap">
+    <div id="sidebar">
+      <h1>Top de Peliculas </h1>
+      <div class="left-box">
+        <ul class="sidemenu">
+          <li><a href="#"></a>Star war </li>
+          <li>Tron</li>
+          <li>Doce monos </li>
+          <li>Alien</li>
+          <li>Super man </li>
+        </ul>
+      </div>
+      </div>
+    <div id="main">
+      <h1>Consulta</h1>
+      <p><strong>Aqui encontraras los titulos de las peliculas de mayor exito a nivel nacional </strong></p>
+      <p>Te invitamos a que te suscribas </p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <br />
+    </div>
+  </div>
+  <div id="footer">
+    <p>P&agrave;gina CSS descargada de https://www.free-css.com/</p>
+  </div>
+</div>
+</body>
+</html>
